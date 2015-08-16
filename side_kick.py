8b6b385a8ff354db000a9ba09febeb7e88a69b8d
MikeThunder <michaeljaredsmith@hotmail.com>
2015-08-16 16:00:33 +1200

    Added attributes
    To class Leader, Sidekick, Ally, Follower
    added class constants for level, size, and basehealth

--- side_kick.py
+++ side_kick.py
@@ -4,5 +4,9 @@ from character import Character
 
 
 class SideKick(Character):
+    level = 3
+    size = 3
+    base_health = 8
+    
     def __init__(self, league, name, health, brawl, shoot, dodge, might, finesse, cunning, **abilities):
         Character.__init__(self, league, name, health, brawl, shoot, dodge, might, finesse, cunning, **abilities)
