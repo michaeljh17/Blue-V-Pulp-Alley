d2bf94e8ab1868359fc02f97ed47ae66662c3dd3
MikeThunder <michaeljaredsmith@hotmail.com>
2015-08-21 14:59:16 +1200

    Pep8
    Making pep8 compliant

--- Tests/Test_Add_Character.py
+++ Tests/Test_Add_Character.py
@@ -1,61 +1,73 @@
 import sys
 import os
 path = os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))
-if not path in sys.path:
+if path not in sys.path:
     sys.path.insert(1, path)
 del path
 from league import *
 from character import *
 from league_model import *
 
+
 class Test_Add_Character(object):
-    
+
     def __init__(self):
         self.lm = LeagueModel()
         self.lm.add_league("Test League")
         self.lm.set_abilities_file(self.lm.read_file("..\Abilities.txt"))
 
-
     def check_character_creation(self, characters_name):
         x = self.lm.get_current_league().find_character(characters_name)
-        if (x == None):
+        if (x is None):
             return False
         else:
             return True
-        
+
     def add_bruce(self):
-        self.lm.get_current_league().add_character("Bruce", char_type="Leader", health="d10",
-                                      brawl="3d10", shoot="2d10", dodge="3d8",
-                                      might="3d10", finesse="2d10",
-                                      cunning="3d8", arg1="Mighty",
-                                      arg2="Brash", arg3="Crafty")
-        
+        self.lm.get_current_league().add_character("Bruce", char_type="Leader",
+                                                   health="d10", brawl="3d10",
+                                                   shoot="2d10", dodge="3d8",
+                                                   might="3d10",
+                                                   finesse="2d10",
+                                                   cunning="3d8",
+                                                   arg1="Mighty", arg2="Brash",
+                                                   arg3="Crafty")
+
     def delete_bruce(self):
         self.lm.get_current_league().delete_character_by_name("Bruce")
-            
-    def check_character_creation_methods(self, tests_name, expectation, name, char_type, health, brawl,
-                      shoot, dodge, might, finesse,
-                      cunning, *abilities):
+
+    def check_character_creation_methods(self, tests_name, expectation, name,
+                                         char_type, health, brawl, shoot,
+                                         dodge, might, finesse, cunning,
+                                         *abilities):
         print("Beginning Test: " + tests_name)
-        self.lm.get_current_league().add_character(name, char_type, health, brawl,
-                      shoot, dodge, might, finesse,
-                      cunning, arg1 = abilities[0], arg2 = abilities[1], arg3 = abilities[2])
+        self.lm.get_current_league().add_character(name, char_type, health,
+                                                   brawl, shoot, dodge, might,
+                                                   finesse, cunning,
+                                                   arg1=abilities[0],
+                                                   arg2=abilities[1],
+                                                   arg3=abilities[2])
         if (self.check_character_creation(name) == expectation):
             print("End of Test: " + tests_name + " Result - Passed")
         else:
             print("End of Test: " + tests_name + " Result - Failed")
         self.lm.get_current_league().delete_character_by_name(name)
         print(" ")
-        
-    def add_character_then_run_creation_method_check(self, tests_name, expectation,
-                                                     name, char_type, health, brawl,
-                                                     shoot, dodge, might, finesse,
-                                                     cunning, *abilities):
+
+    def add_character_then_run_creation_method_check(self, tests_name,
+                                                     expectation, name,
+                                                     char_type, health, brawl,
+                                                     shoot, dodge, might,
+                                                     finesse, cunning,
+                                                     *abilities):
         print("Beginning Test: " + tests_name)
         self.add_bruce()
-        self.lm.get_current_league().add_character(name, char_type, health, brawl,
-                      shoot, dodge, might, finesse,
-                      cunning, arg1 = abilities[0], arg2 = abilities[1], arg3 = abilities[2])
+        self.lm.get_current_league().add_character(name, char_type, health,
+                                                   brawl, shoot, dodge, might,
+                                                   finesse, cunning,
+                                                   arg1=abilities[0],
+                                                   arg2=abilities[1],
+                                                   arg3=abilities[2])
         self.delete_bruce()
         if (self.check_character_creation(name) == expectation):
             print("End of Test: " + tests_name + " Result - Passed")
@@ -63,4 +75,3 @@ class Test_Add_Character(object):
             print("End of Test: " + tests_name + " Result - Failed")
         self.lm.get_current_league().delete_character_by_name(name)
         print(" ")
-        
--- Tests/Test_Add_Leader.py
+++ Tests/Test_Add_Leader.py
@@ -5,25 +5,32 @@ test = Test_Add_Character()
 
 """ Test leader Creation """
 test.check_character_creation_methods("Create a Leader, Happy Day", True,
-                                      "Bruce","Leader","d10","3d10","3d10",
-                                      "3d8","3d10","2d8","2d10", "Mighty", 
+                                      "Bruce", "Leader", "d10", "3d10", "3d10",
+                                      "3d8", "3d10", "2d8", "2d10", "Mighty",
                                       "Brash", "Crafty")
 
 test.check_character_creation_methods("Create a Leader, Incorrectly Set Skill",
-                                False,"Bruce", "Leader","d10","2d10","3d10",
-                                "3d8","3d10","2d8","2d10","Mighty","Brash",
-                                "Crafty")
+                                      False, "Bruce", "Leader", "d10", "2d10",
+                                      "3d10", "3d8", "3d10", "2d8", "2d10",
+                                      "Mighty", "Brash", "Crafty")
 
-test.check_character_creation_methods("Create a Leader, Incorrectly Set Health",
-                                False,"Bruce", "Leader","d8","3d10","3d10",
-                                "3d8","3d10","2d8","2d10","Mighty","Brash",
-                                "Crafty")
+test.check_character_creation_methods("Create a Leader, Incorrectly Set" +
+                                      " Health", False, "Bruce", "Leader",
+                                      "d8", "3d10", "3d10", "3d8", "3d10",
+                                      "2d8", "2d10", "Mighty", "Brash",
+                                      "Crafty")
 
-test.add_character_then_run_creation_method_check("Create a Leader, When there is already a leader",
-                                False,"Danny", "Leader","d10","3d10","3d10",
-                                "3d8","3d10","2d8","2d10","Mighty","Brash",
-                                "Crafty")
+test.add_character_then_run_creation_method_check("Create a Leader, When " +
+                                                  "there is already a leader",
+                                                  False, "Danny", "Leader",
+                                                  "d10", "3d10", "3d10", "3d8",
+                                                  "3d10", "2d8", "2d10",
+                                                  "Mighty", "Brash", "Crafty")
 
-test.add_character_then_run_creation_method_check("Create a Leader, When there is already a leader with the same name", 
-                                False,"Bruce", "Leader","d10","3d10","3d10","3d8","3d10","2d8",
-                                "2d10","Mighty","Brash","Crafty")
+test.add_character_then_run_creation_method_check("Create a Leader, When " +
+                                                  "there is already a leader" +
+                                                  " with the same name", False,
+                                                  "Bruce", "Leader", "d10",
+                                                  "3d10", "3d10", "3d8",
+                                                  "3d10", "2d8", "2d10",
+                                                  "Mighty", "Brash", "Crafty")
